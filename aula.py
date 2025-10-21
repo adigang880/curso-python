9a6806213d2320ad3fbba5b680cd92cdb46428cc
Arthur <arthurribeiro.ufsj@gmail.com>
2025-10-20 21:55:41 -0300

    zero

--- aula.py
+++ aula.py
@@ -1,3 +1,11 @@
-a = 1
-b = a
-print (b)
+lista_alunos = []
+novo = {"nome": 'Maria', "nota": 9.2}
+novo['curso'] = "Engenharia"
+
+lista_alunos.append(novo)
+novo = {"nome": 'joao', "nota": 8.2}
+lista_alunos.append(novo)
+print(lista_alunos[1]['nota'])
+
+for i in range(2):
+    print(lista_alunos[i]['nome'])

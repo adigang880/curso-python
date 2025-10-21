5120f3b28e2dee40fd20f6a8e2cb42729ea190c8
Arthur <arthurribeiro.ufsj@gmail.com>
2025-10-20 22:02:27 -0300

    soma

--- aula.py
+++ aula.py
@@ -1,11 +1,3 @@
-lista_alunos = []
-novo = {"nome": 'Maria', "nota": 9.2}
-novo['curso'] = "Engenharia"
-
-lista_alunos.append(novo)
-novo = {"nome": 'joao', "nota": 8.2}
-lista_alunos.append(novo)
-print(lista_alunos[1]['nota'])
-
-for i in range(2):
-    print(lista_alunos[i]['nome'])
+a = 1
+b = 2
+print(a+b)
